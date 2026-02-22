956222c12a1dc05c6a032ef3d4dee901710290a9
Henrique Caltram <henrivai@hotmail.com>
2026-02-22 17:22:30 -0300

    (fix) Allos same-origin requests

--- api/app/main.py
+++ api/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routes.auth import router as auth_router
 from app.routes.brewings import router as brewings_router
@@ -6,6 +7,14 @@ from app.routes.coffee_brands import router as coffee_brands_router
 from app.routes.coffees import router as coffees_router
 
 app = FastAPI(title="Coffee Exp API", version="0.1.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router)
 app.include_router(coffee_brands_router)
