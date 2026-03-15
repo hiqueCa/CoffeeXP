e8ca2509fe7de9fd7b40d4d5b324c3d3ab3f3df3
Henrique Caltram <henrivai@hotmail.com>
2026-03-15 13:48:57 +0100

    (refactor) Remove BaseModel

--- api/app/main.py
+++ api/app/main.py
@@ -3,8 +3,6 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from app.routes.auth import router as auth_router
 from app.routes.brewings import router as brewings_router
-from app.routes.coffee_brands import router as coffee_brands_router
-from app.routes.coffees import router as coffees_router
 
 app = FastAPI(title="Coffee Exp API", version="0.1.0")
 
@@ -17,8 +15,6 @@ app.add_middleware(
 )
 
 app.include_router(auth_router)
-app.include_router(coffee_brands_router)
-app.include_router(coffees_router)
 app.include_router(brewings_router)
 
 
