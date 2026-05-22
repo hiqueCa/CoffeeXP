c2fd1455e86f9c73e10952db368a3867bd036ee7
Henrique Caltram <henrivai@hotmail.com>
2026-05-22 17:41:31 +0200

    Simplify button stories

--- web/src/components/atoms/Button.stories.tsx
+++ web/src/components/atoms/Button.stories.tsx
@@ -1,10 +1,6 @@
 import { type Story } from '@ladle/react';
 import { Button } from './Button';
 
-export const Text: Story = () => (
-  <Button onClick={() => alert('Button clicked!')}>Click me</Button>
-);
-
 export const Contained: Story = () => (
   <Button onClick={() => alert('Button clicked!')} variant="contained">
     Click me
