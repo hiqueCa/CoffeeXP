fcd5df3e79b023a20d6255ece41a6fe50af7a233
Henrique Caltram <henrivai@hotmail.com>
2026-06-10 10:33:56 +0200

    Rename Text to Typography to match Mui 1:1 parity convention

--- web/src/components/atoms/__tests__/Typography.test.tsx
+++ web/src/components/atoms/__tests__/Typography.test.tsx
@@ -1,10 +1,10 @@
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import { Text } from '../text/Text';
+import { Typography } from '../typography/Typography';
 
-describe('Text', () => {
+describe('Typography', () => {
 	it('renders the specified label', async () => {
-		render(<Text label={'Lorem Ipsum Dolor'} />);
+		render(<Typography label={'Lorem Ipsum Dolor'} />);
 
 		expect(await screen.findByText('Lorem Ipsum Dolor')).toBeInTheDocument();
 	});
--- web/src/components/atoms/text/Text.tsx
+++
@@ -1,9 +0,0 @@
-import { Typography } from '@mui/material';
-
-interface ITextProps {
-	label: string;
-}
-
-export const Text = ({ label }: ITextProps) => {
-	return <Typography>{label}</Typography>;
-};
