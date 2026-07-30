2b21ad93dbf16f3cd3414f46de391745496d6735
Henrique Caltram <henrivai@hotmail.com>
2026-07-30 13:59:52 +0200

    Initialize vite tanstack config

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,8 +1,20 @@
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
+  /**
+   * As by documentation, Tanstack vite plugin needs to come before react():
+   * https://tanstack.com/router/latest/docs/installation/with-vite
+   */
+  plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+      quoteStyle: "double",
+    }),
+    react(),
+  ],
   server: {
     port: 3000,
   },
