f45053cd759403937a25933116cb39888da6c3c0
Henrique Caltram <henrivai@hotmail.com>
2026-09-05 15:28:03 +0200

    Flatten tsconfig package out of tooling package and fix typecheck dependency errors

--- packages/auth/auth.ts
+++ packages/auth/auth.ts
@@ -1,8 +1,8 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
-export const auth = betterAuth({
-  database: drizzleAdapter(db, {
-    provider: "pg",
-  }),
-});
+// export const auth = betterAuth({
+//   database: drizzleAdapter(db, {
+//     provider: "pg",
+//   }),
+// });
